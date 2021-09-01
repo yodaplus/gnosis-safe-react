0e951bf780868d8239dc042d9943f654e4a8efd4
Taras Yevtushenko <taras.evt@gmail.com>
2021-09-01 14:03:53 +0300

    Switch off no-unused-vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,7 @@ module.exports = {
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
+    '@typescript-eslint/no-unused-vars': 'off',
   },
   settings: {
     react: {
