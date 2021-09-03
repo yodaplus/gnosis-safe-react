5d98fca034f513700f08a20b606c35fbcf6c247f
Taras Yevtushenko <taras.evt@gmail.com>
2021-09-03 10:19:09 +0300

    Deployment script update

--- scripts/deploy_ecr.sh
+++ scripts/deploy_ecr.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+docker context use default
+
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 135135176603.dkr.ecr.ap-south-1.amazonaws.com
+
 REACT_APP_NETWORK=apothem REACT_APP_ENV=production yarn build
 docker build -f docker/Dockerfile -t 135135176603.dkr.ecr.ap-south-1.amazonaws.com/safe-transaction-service_front:apothem .
 docker push 135135176603.dkr.ecr.ap-south-1.amazonaws.com/safe-transaction-service_front:apothem
